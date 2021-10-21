ab86b59fac402c09d563058cd49d1d8865131ce2
WabbyBabbo <1162287492@qq.com>
2021-10-21 18:17:16 +0800

    update assign4-bonus

--- Assignment4/code/main.cpp
+++ Assignment4/code/main.cpp
@@ -51,7 +51,7 @@ void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
 
-    for(float t = 0.0; t<=1.0; t+=0.002) {
+    for(float t = 0.0; t<=1.0; t+=0.001) {
         auto point = recursive_bezier(control_points, t);
         int x_floor = std::floor(point.x);
         int y_floor = std::floor(point.y);
