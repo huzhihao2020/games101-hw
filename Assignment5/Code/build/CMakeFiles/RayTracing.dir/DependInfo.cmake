a3084292559f25c3b03d794a7028253247e35e0c
WabbyBabbo <1162287492@qq.com>
2021-10-25 16:59:32 +0800

    Init assign5

---
+++ Assignment5/Code/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lance/code/GitHub/games101-hw/Assignment5/Code/Renderer.cpp" "/Users/lance/code/GitHub/games101-hw/Assignment5/Code/build/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/Users/lance/code/GitHub/games101-hw/Assignment5/Code/Scene.cpp" "/Users/lance/code/GitHub/games101-hw/Assignment5/Code/build/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/Users/lance/code/GitHub/games101-hw/Assignment5/Code/main.cpp" "/Users/lance/code/GitHub/games101-hw/Assignment5/Code/build/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
